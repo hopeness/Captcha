9144b9e402a38c211ba192ea53217ad3147d0e2f
Hopeness <houpeng1234@126.com>
2014-07-20 19:36:14 +0800

    Format adjustment

--- Captcha.class.php
+++ Captcha.class.php
@@ -3,95 +3,95 @@
  * 通用验证码生成类
  **/
 class Captcha {
-    
+
     private $obj;
-    
+
     public function __construct($width = 80, $height = 35, $fontSize = null, $overlap = 0.7, $angle = 30){
         $this->obj = new Captcha_GD($width, $height, $fontSize, $overlap, $angle);
     }
-    
+
     public function show($code){
         $this->obj->show($code);
     }
-    
+
 }
 
 
 /**
- * GD验证码声场类
+ * GD验证码生成类
  **/
 class Captcha_GD {
-    
+
     /**
      * 画布资源
      **/
     private $image;
-    
+
     /**
      * 验证码字符串
      **/
     private $code;
-    
+
     /**
      * 字符串总数
      **/
     private $codeCount;
-    
+
     /**
      * 图像宽度
      **/
     private $width;
-    
+
     /**
      *图像高度
      **/
     private $height;
-    
+
     /**
      * 图像背景色
      **/
     private $bgColor;
-    
+
     /**
      * 字体大小
      **/
     private $fontSize;
-    
+
     /**
      * 字体颜色
      **/
     private $fontColor;
-    
+
     /**
      * 字体重叠比
      **/
     private $overlap;
-    
+
     /**
      * 字体文件地址
      **/
     private $font;
-    
+
     /**
      * 字体包路径
      **/
     private $fontDir;
-    
+
     /**
      * 字体库
      **/
     private $fontArray;
-    
+
     /**
      * 向左最大旋转角度
      **/
     private $maxAngle;
-    
+
     /**
      * 向右旋转角度
      **/
     private $minAngle;
-    
+
     public function __construct($width = 80, $height = 35, $fontSize = null, $overlap = 0.7, $angle = 30){
         $this->width = $width;
         $this->height = $height;
@@ -101,7 +101,7 @@ class Captcha_GD {
         $this->minAngle = -$angle;
         $this->fontDir = dirname(__FILE__) . '/font/';
     }
-    
+
     public function show($code){
         $this->code = $code;
         $this->codeCount = strlen($this->code);
@@ -113,13 +113,13 @@ class Captcha_GD {
                 $this->fontSize = $this->height * 1.2;
             }
         }
-        
+
         $this->image = imagecreate($this->width, $this->height);
         $this->bgColor = imagecolorallocate($this->image, 70, 100, 100);
         $this->fontColor = imagecolorallocate($this->image, 255, 255, 255);
         $this->kuang = imagecolorallocate($this->image, 0, 0, 0);
         $this->font = $this->randomFont();
-        
+
         // 为每个字符单独设置旋转角度、大小等
         $left = 0;
         for($i=0; $i<$this->codeCount; $i++){
@@ -144,11 +144,11 @@ class Captcha_GD {
                     }
                 }
             }
-            
+
             // 计算图形边长
             $width = abs($x['min'] - $x['max']);
             $height = abs($y['min'] - $y['max']);
-            
+
             // 图像边缘距离
             $top = ($this->height - $height) / 2 + abs($y['min']);
             if($left == 0){
@@ -156,9 +156,9 @@ class Captcha_GD {
             }else{
                 $left = ($left + $width * $this->overlap);
             }
-           
+
             $kuang = imagettftext($this->image, $this->fontSize, $angle, $left, $top, $this->fontColor, $this->font, $this->code[$i]);
-            
+
             // 调试信息
             // 字符坐标排查，插入自定义header
             // header('X-test1: '.$angle.' | '.$width.' | '.$height.' | '.$left.' | '.$top.' |$| (x'.$kuang[0].' y'.$kuang[1].') | (x'.$kuang[2].' y'.$kuang[3].') | (x'.$kuang[4].' y'.$kuang[5].') | (x'.$kuang[6].' y'.$kuang[7].')');
@@ -173,14 +173,14 @@ class Captcha_GD {
             // imagepolygon($this->image, $kuang, 4, $this->kuang);
             // imagepolygon($this->image, array($x['min'], $y['min'], $x['max'], $y['min'], $x['max'], $y['max'], $x['min'], $y['max']), 4, $this->kuang);
         }
-        
+
         header('Content-Type: image/png');
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
         header("Pragma: no-cache");
         header('Expires: 0');
         imagepng($this->image);
     }
-    
+
     private function randomFont(){
         if(empty($this->fontArray)){
             if(false != ($handle = opendir($this->fontDir))){
@@ -198,18 +198,18 @@ class Captcha_GD {
         $key = array_rand($fileArray);
         return $fileArray[$key];
     }
-    
+
     public function __destruct(){
         if($this->image){
             imagedestroy($this->image);
         }
     }
-    
+
 }
 
 
 /**
- * Imagic验证码声场类
+ * Imagic验证码生成类
  **/
 class Captcha_Imagick {
     
